1ca3941e53811b52960287b86c7914e05a237dd7
WgDksilva <wagnersilva.dk021@gmail.com>
2024-11-21 00:52:32 -0300

    Projeto Random finalizado

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const botaoSorteio = document.querySelector(".botao-sorteio")
+
+function gerarNúmero(){
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+    const numeroSortiado =document.querySelector(".input-n-sortiado")
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    numeroSortiado.innerHTML = result
+
+}
+
+botaoSorteio.addEventListener("click", gerarNúmero)
